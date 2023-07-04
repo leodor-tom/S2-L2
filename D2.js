2a46863b5f866cd873936347a75e62fe26452e91
leodor-tom <135353580+leodor-tom@users.noreply.github.com>
2023-07-04 15:43:12 +0200

    exercise 10 & 11

--- D2.js
+++ D2.js
@@ -140,8 +140,14 @@ if (evenOrOdd === 0) {
       console.log("Uguale a 10 o maggiore");
     }
 */
-
-/* SCRIVI QUI LA TUA RISPOSTA */
+let val = 7;
+if (val < 5) {
+  console.log(" Meno di 5 ");
+} else if (val < 10) {
+  console.log("Meno di 10");
+} else {
+  console.log("Uguale a 10 o maggiore");
+}
 
 /* ESERCIZIO 11
   Fornito il seguente oggetto, scrivi del codice per aggiungere una proprietà "city", il cui valore sarà "Toronto".
@@ -152,6 +158,10 @@ const me = {
   lastName: "Doe",
   skills: ["javascript", "html", "css"],
 };
+
+me.city = "Toronto";
+
+console.log("me", me);
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
